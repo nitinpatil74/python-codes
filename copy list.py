68142fb697eeb03e938110b70901f5fd33f48421
Nitin Patil <np5088319@gmail.com>
2026-09-26 00:32:48 +0530

    Add files via upload
    copy lists

---
+++ copy list.py
@@ -0,0 +1,9 @@
+favorite_items = ["T-shirt", "Lamp", "Pen"]
+
+cart = favorite_items
+
+# Add an item to favorite_items list
+favorite_items.append("Book")
+
+print(f"favorite_items = {favorite_items}")
+print(f"cart = {cart}")
